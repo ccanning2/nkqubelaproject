4c4454e1cc5211232e17304705b591588d9cc76a
211149365@mycput.ac.za <211149365@mycput.ac.za@7cf9ce4a-2238-7693-fad1-6fb63ce334c6>
2013-10-24 10:46:11 +0000

    - Fixed Patient Test.

--- HospitalManagement/test/PatientTest.java
+++ HospitalManagement/test/PatientTest.java
@@ -3,9 +3,14 @@
  * and open the template in the editor.
  */
 
+import hospital.app.facade.Facade;
 import hospital.app.factory.AppFactory;
+import hospital.model.entities.MedicalAid;
 import hospital.model.entities.Patient;
+import hospital.model.entities.Ward;
+import hospital.services.crud.MedicalAidCrudService;
 import hospital.services.crud.PatientCrudService;
+import hospital.services.crud.WardCrudService;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,6 +31,8 @@ import org.testng.annotations.Test;
 public class PatientTest {
     
     private static PatientCrudService patientCrudService;
+    private static MedicalAidCrudService medicalAidCrudService;
+    private static WardCrudService wardCrudService;
     private static long id;    
     private static ApplicationContext ctx;
     
@@ -36,25 +43,53 @@ public class PatientTest {
     @SuppressWarnings({"null", "ConstantConditions"})
     public void testPatientCreate(){
         System.out.println("---TESTPATIENTCREATE---");
-        Patient patient = null;
+        Patient patient;
+        Ward ward;
+        MedicalAid medicalAid = new MedicalAid();
         
         Map<String, String> stringValues = new HashMap<String, String>();
-       
-        stringValues.put("bedNumber", "1");
-        stringValues.put("dateOfArrival", "24 JUNE 2013");
-        stringValues.put("name", "enzo");
-        stringValues.put("medicalAidId", "1");
-        stringValues.put("patientNumber", "111");
-        stringValues.put("reasonForStay", "CANCER");
         
-       
-        //patient = AppFactory.getPatient(stringValues,1,(long)111, new Date(), new Date(), false);
+        stringValues.put("firstName", "Peter");
+        stringValues.put("lastName", "Jones");
+        stringValues.put("middleName", "Bobby");
+        stringValues.put("nickName", "Pet");
+        stringValues.put("contactNumber", "0726289563");
+        stringValues.put("emailAddress", "peterb@jones.co.za");
+        stringValues.put("gender", "Male");
+        stringValues.put("race", "White");
+        stringValues.put("title", "Dr");
+        stringValues.put("identityNumber", "8512206201951");
+        stringValues.put("currentCondition", "Critical");
+
+        stringValues.put("medicalAidName", "Dicovery Health");
+        stringValues.put("medicalAidScheme", "Full");
+        stringValues.put("reasonForStay","Heart Transplant");  
         
-        //patientCrudService.persist(patient);
+        Map<String, Long> longValues = new HashMap<String, Long>();
         
-        //id = patient.getId();
+        longValues.put("medicalAidNumber", (long)025632);
+        longValues.put("bedNumber", (long)4588);
+        longValues.put("patientNumber", (long)9515);
         
-        //System.out.println("testPatientCreate: id = " + id);
+        Map<String, Date> dateValues = new HashMap<String, Date>();
+        
+        dateValues.put("dateOfArrival", new Date());
+        dateValues.put("dateOfBirth", new Date());
+        dateValues.put("estimatedDateOfDischarge", new Date());
+    
+        medicalAid.setMedicalAidName(stringValues.get("medicalAidName"));
+        medicalAid.setMedicalAidNumber(longValues.get("medicalAidNumber"));
+        medicalAid.setMedicalAidScheme(stringValues.get("medicalAidScheme"));
+        
+        medicalAidCrudService.persist(medicalAid);
+        
+        ward = wardCrudService.findById((long)1);                
+        
+        patient = AppFactory.getPatient(stringValues, longValues, dateValues, Boolean.TRUE, ward);
+
+        patientCrudService.persist(patient);
+        
+        id = patient.getId();
     }
     
     @Test(priority = 2)
@@ -70,21 +105,19 @@ public class PatientTest {
         System.out.println("medicalAidId: " + patient.getMedicalAid());
         System.out.println("patientNumber: " + patient.getPatientNumber());
         System.out.println("reasonForStay: " + patient.getReasonForStay());
-        
-        Assert.assertEquals(patient.getId(), "4");
     }
     
     @Test(priority = 3)
     public void testPatientUpdate(){
         System.out.println("---TESTPATIENTUPDATE---");
-       Patient patient = patientCrudService.findById(id);
+        Patient patient = patientCrudService.findById(id);
         
-        patient.setCurrentCondition("CANCER");
-        patient.getName().setFirstName("JOHN");
+        patient.setCurrentCondition("AIDS");
+        patient.getName().setFirstName("John");
         
         patientCrudService.merge(patient);
         
-        Assert.assertEquals(patient.getFullName(), "JOHN");
+        Assert.assertEquals(patient.getName().getFirstName(), "John");
     }
     
     @Test(enabled = false, priority = 4)
@@ -100,6 +133,8 @@ public class PatientTest {
     public static void setUpClass() throws Exception {
         ctx = new ClassPathXmlApplicationContext("classpath:hospital/app/config/applicationContext-connection.xml");
         patientCrudService = (PatientCrudService) ctx.getBean("patientCrudService");
+        medicalAidCrudService = (MedicalAidCrudService) ctx.getBean("medicalAidCrudService");
+        wardCrudService = (WardCrudService) ctx.getBean("wardCrudService");
     }
 
     @AfterClass
